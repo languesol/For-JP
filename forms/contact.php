07407e9ed728fd2225c428d9abdcce6c2bfd534d
languesol <75699323+languesol@users.noreply.github.com>
2020-12-10 04:26:23 +0100

    more contact changes

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@ if (isset ($_POST['Send message'])) {
   $message = $_Post ['Message'];
 
 
-  $mailTo = "hi@jeanpierremurray.com"
+  $mailTo = "hi@jeanpierreùurray.com"
   $headers ="From: ". $mailFrom;
   $txt = "You have received an email from ". $name. ".\n\n". $message;
 
