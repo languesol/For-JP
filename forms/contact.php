3036ee4b38c5c799d1de56c31c3e6ef7b019ea71
languesol <75699323+languesol@users.noreply.github.com>
2020-12-10 02:40:22 +0100

    JPLOVE

---
+++ forms/contact.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset ($_POST['Send message'])) {
+  $name = $_Post ['Your name'];
+  $email = $_Post ['Your email'];
+  $subject = $_Post ['subject'];
+  $message = $_Post ['Message'];
+
+
+  $mailTo = "rhodyann@languesolinternational.com"
+  $headers ="From: ". $mailFrom;
+  $txt = "You have received an email from ". $name. ".\n\n". $message;
+
+  mail($mailTo, $subject, $txt, $headers);
+  header ("Location: index.php?mailsend");
+}
