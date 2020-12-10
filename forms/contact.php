e825ac89320eb21442f0bb4c8258729e88610ff6
languesol <75699323+languesol@users.noreply.github.com>
2020-12-10 05:19:35 +0100

    changesagain

--- forms/contact.php
+++ forms/contact.php
@@ -1,16 +1,17 @@
 <?php
 
-if (isset ($_POST['Send message'])) {
-  $name = $_Post ['Your name'];
-  $email = $_Post ['Your email'];
-  $subject = $_Post ['subject'];
+if (isset($_POST['submit'])) {
+  $name = $_Post ['name'];
+  $subject =_Post ['subject'];
+  $mailFrom = $_Post ['mail'];
   $message = $_Post ['Message'];
+}
 
 
-  $mailTo = "hi@jeanpierreùurray.com"
+  $mailTo = "hi@jeanpierremurray.com";
   $headers ="From: ". $mailFrom;
   $txt = "You have received an email from ". $name. ".\n\n". $message;
 
   mail($mailTo, $subject, $txt, $headers);
-  header ("Location: index.php?mailsend");
+  header ("Location: index.html?mailsend");
 }
